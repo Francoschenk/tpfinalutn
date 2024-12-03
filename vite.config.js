e5ce463563b2ba80472ee77b8e873650222a3e82
franco schenk <schenkfranco@gmail.com>
2024-12-03 15:34:30 -0300

    server

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    host:"0.0.0.0",
+    port:1000
+  },
   plugins: [react()],
 })
